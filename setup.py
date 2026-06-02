8895d4e39a97521174f1bcf271040e0928ffe56a
naimur005 <naimur005@gmail.com>
2026-06-03 01:29:09 +0900

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Md Naimur Rahman",
+    author_email="naimur005@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
